3e0739748e4b68ace5a17ba8a24d8a51ed9e6cdb
Hasna Hena Mow <hasnahenamow@gmail.com>
2024-12-09 01:04:10 +0600

    global error handling with middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 
+import globalErrorHandler from './middleware/globalErrorHandler.js';
 import { UserRoutes } from './modules/users/users.route.js';
 
 const app = express();
@@ -16,5 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users', UserRoutes);
+
+app.use(globalErrorHandler);
 
 export { app };
